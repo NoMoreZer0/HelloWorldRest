5338348b791ec426b75a87c835725aa5a2d9dbee
NoMoreZer0 <kazakupersuper@mail.ru>
2023-10-26 13:54:41 +0600

    first commit

---
+++ src/main/java/com/example/helloworlddemomaven/HelloWorldDemoMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworlddemomaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldDemoMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldDemoMavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/helloworlddemomaven/HelloWorldDemoMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworlddemomaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldDemoMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
